67203776b59d387505481491f57972b233a06e58
Yunzhu Li <contact@yunzhu.li>
2019-02-19 16:21:37 -0500

    repos

--- gh.tf
+++ gh.tf
@@ -2,6 +2,7 @@ provider "github" {
   organization = "case-fold"
 }
 
+# Teams and memberships
 resource "github_team" "test_team_0" {
   name        = "test-team-0"
   description = "Cool team created by tf"
@@ -17,5 +18,23 @@ resource "github_team_membership" "test_team_0_ling" {
 resource "github_team_membership" "test_team_0_yl" {
   team_id  = "${github_team.test_team_0.id}"
   username = "yunzhu-li"
-  role     = "member"
+  role     = "maintainer"
+}
+
+# Repos
+resource "github_repository" "tf_test" {
+  name        = "tf-test"
+  description = "Repo managed by Terraform"
+}
+
+resource "github_repository_collaborator" "tf_test_collaborator" {
+  repository = "${github_repository.tf_test.name}"
+  username   = "gkilleen"
+  permission = "admin"
+}
+
+resource "github_team_repository" "some_team_repo" {
+  team_id    = "${github_team.test_team_0.id}"
+  repository = "${github_repository.tf_test.name}"
+  permission = "admin"
 }
